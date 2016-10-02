fa82ddf7f469d808693c2c4beb588702dae65bab
SherinJames <sherinjames94bangalore@gmail.com>
2016-10-02 14:45:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,9 +110,9 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name', function (req, res) {
+app.get('/submit-name'/:name, function (req, res) {
     // get the name from the request
-    var name = req.params.name; // TODO
+    var name = req.params.name; 
     
     names.push(name);
     //JSON js obj notation
